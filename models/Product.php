f0ad0409d05f25a3a4be0541beb006aa5941d9b0
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-27 09:48:14 +0200

    creazione costruttore Product

--- models/Product.php
+++ models/Product.php
@@ -6,8 +6,15 @@ class Product
     public $brand;
     public $name;
     public $description = '';
-    public $category;
+    public $category = '';
 
     public $price = 0;
     public $vat = 22;
+
+    function __construct($_brand, $_name, $_price)
+    {
+        $this->brand = $_brand;
+        $this->name = $_name;
+        $this->price = $_price;
+    }
 }
