6a7484073a3ae851b06a18b85404a4186ee52386
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-27 10:30:58 +0200

    definizione checkOut di RegisteredUser

--- models/RegisteredUser.php
+++ models/RegisteredUser.php
@@ -7,4 +7,14 @@ class RegisteredUser extends User
     public $surname;
     public $password;
     public $profileImage;
+
+    public function checkOut()
+    {
+        $totalCartPrice = parent::checkOut();
+        return [
+            "total" => $totalCartPrice['total'] * 0.8,
+            "vat" => $totalCartPrice['vat'] * 0.8,
+            "granTotal" => ($totalCartPrice['total'] + $totalCartPrice['vat']) * 0.8,
+        ];
+    }
 }
