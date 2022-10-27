cf89f03f5862d7bc8e12585bc757493f95c0468d
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-27 10:16:36 +0200

    creazione classe User

---
+++ models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+class User
+{
+    public $email;
+    public $address = '';
+
+    public $creditCardNumber;
+    public $creditCardOwner;
+    public $creditCardExpireDate;
+    public $creditCardCVV;
+}
