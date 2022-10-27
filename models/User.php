eee787996c246d9c068bb806611b4f2b9a321d8b
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-27 10:25:07 +0200

    creazione metodi + var in classe User

--- models/User.php
+++ models/User.php
@@ -9,4 +9,28 @@ class User
     public $creditCardOwner;
     public $creditCardExpireDate;
     public $creditCardCVV;
+
+    public $cart = [];
+
+    public function addProduct($product)
+    {
+        $this->cart[] = $product;
+    }
+
+    public function checkOut()
+    {
+        $totalPrice = 0;
+        $totalVat = 0;
+
+        foreach ($this->products as $product) {
+            $totalPrice += $product->price;
+            $totalVat += ($totalPrice * $product->vat / 100);
+        }
+
+        return [
+            "total" => $totalPrice,
+            "vat" => $totalVat,
+            "granTotal" => $totalPrice + $totalVat
+        ];
+    }
 }
